d131e6c5e83d46ee53d4c528f24b41d2d699a8cc
Adeilson Leal <adeilson.leal@ee.ufcg.edu.br>
2022-10-15 17:20:29 -0300

    Calculo dos indices de refracao

---
+++ Reflectivity.py
@@ -0,0 +1,46 @@
+
+
+from cmath import cos, pi, sin, sqrt
+
+from numpy import array
+
+
+def Reflectivity(nLayers, d,  index, theta_i, wavelenght):
+
+        j = complex(0, 1)  # Simplification for the complex number "j"
+        k0 = (2 * pi) / wavelenght  # Wave number
+
+        b = []  # b_j -> Phase shift in each layer
+        q_TM = []  # q_TM_j -> Admittance in TM polarization
+
+        M_Tm = []  # M_Tm_j -> Transfer matrix between each layer - TM polarization
+       
+        for layer in range(nLayers):
+            y = sqrt((index[layer] ** 2) - ((index[0] * sin(theta_i)) ** 2))
+
+            b.append(k0 * d[layer] * y)
+            q_TM.append(y / index[layer] ** 2)
+
+
+            # Total Transfer Matrix
+            if layer < (nLayers - 1):
+                M_Tm.append(array([[cos(b[layer]), (-j / q_TM[layer]) * sin(b[layer])],
+                                   [-j * q_TM[layer] * sin(b[layer]), cos(b[layer])]]))
+                
+
+        Mt_TM = M_Tm[0]  # Mt_TM -> Total Transfer Matrix - TM polarization
+        
+        for k in range(nLayers - 2):
+            Mt_TM = Mt_TM @ M_Tm[k + 1]
+
+        num_TM = (Mt_TM[0][0] + Mt_TM[0][1] * q_TM[nLayers - 1]) * q_TM[0] - (
+                Mt_TM[1][0] + Mt_TM[1][1] * q_TM[nLayers - 1])
+        den_TM = (Mt_TM[0][0] + Mt_TM[0][1] * q_TM[nLayers - 1]) * q_TM[0] + (
+                Mt_TM[1][0] + Mt_TM[1][1] * q_TM[nLayers - 1])
+
+        
+
+        r_TM = num_TM / den_TM  # 'r_TM'-> Fresnel reflection coefficient - TM polarization
+
+        return abs(r_TM) ** 2  # Reflectance - TM polarization
+      
--- Simulador SPR.py
+++ Simulador SPR.py
@@ -6,12 +6,15 @@ from cmath import nan, pi
 import numpy as np
 import os
 import Setting_Layers as sl
+import Reflectivity as ref
+import matplotlib.pyplot as plt
 
 ACF = (pi/180)  # Angle Conversion Factor.
 
 mod_int = nan   # Interrogation mode
 lambda_i = nan  # Incident wavelength
 theta_i = nan   # Angle of incidence
+R_Tm = []       # Stores lists with reflectivity curves for each of the interactions    
 
 # screen cleaning
 if os.name == 'nt':
@@ -40,7 +43,7 @@ if mod_int == 1:
           "\n## Setting angular interrogation mode:\n\n ")
 
     # Incident wavelength
-    lambda_i = float(input("Incident wavelength (nm): "))
+    lambda_i = float(input("Incident wavelength (nm): ")) * 1e-9
 
     # Starting and ending angle
     a1 = float(input("Starting angle value (degress): "))*ACF
@@ -48,16 +51,50 @@ if mod_int == 1:
 
     theta_i = np.arange(a1, a2, 0.001*ACF)  # Array with angles of incidence
 
-    print(
-        f"\nlen(theta_i) = {len(theta_i)}\n theta_i = {theta_i} \nIncidet wavelength = {lambda_i} nm \n a1 = {a1/ACF} deg. and a2 = {a2/ACF} deg.\n")
-
     #  Defining the structure
-    n_layers = int(
-        input("\nSet the number of layers in your structure:\n     N = "))
+    n_layers = int(input("\nSet the number of layers in your structure:\n     N = "))
 
-    d, material = sl.setLayers(n_layers)
+    d, ref_index = sl.setLayers(n_layers, lambda_i)
 
-    print(f"d = {d}\nMaterial = {material}")
+    n_subs = int(input("\nHow many substances to analyze in the recipe?\n >> "))
+    
+    list_analyte = sl.setAnalyte(n_subs)
+
+    for i in range(n_subs):
+        r_TM = []      # Local variable that temporarily stores reflectivity values for each of the interactions
+        
+        if i == 0:
+            d.append(1)
+            ref_index.append(sl.set_index(16,lambda_i))
+            
+            for t in range(len(theta_i)):
+               r_TM.append(ref.Reflectivity(n_layers , d, ref_index, theta_i[t], lambda_i,))
+            
+            R_Tm.append(r_TM)
+        
+        else:
+            ref_index[-1] = complex(list_analyte[i])
+
+            for t in range(len(theta_i)):
+               r_TM.append(ref.Reflectivity(n_layers , d, ref_index, theta_i[t], lambda_i,))
+            
+            R_Tm.append(r_TM)
+              
+        print(f"d = {d}\nRefractive Index = {ref_index}")
+
+
+    y = 'Angle'
+    c = 'Degrees'
+    n = chr(952)
+    ax_x = theta_i / ACF
+    fig0, ax_TM = plt.subplots()
+    ax_TM.plot(ax_x, (R_Tm[0]))
+    ax_TM.set_title("Reflectivity vs. Angle of Incidence - TM", loc='center', pad='6')
+    ax_TM.set(xlabel=f'Incidence {y} ({c})', ylabel='Reflectivity')
+    
+    plt.yticks(np.arange(0, 1.20, 0.20))
+    ax_TM.grid()
+    plt.show()
 
 elif mod_int == 2:
     #  Setting Wavelength interrogation Mode:
